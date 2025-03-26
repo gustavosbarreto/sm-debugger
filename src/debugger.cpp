b35cd91b1fe80767796d57c7d4e99d999f532c06
ViniciusRed <atalibavinicius@gmail.com>
2025-03-25 22:22:55 -0300

    fix

--- src/debugger.cpp
+++ src/debugger.cpp
@@ -95,7 +95,7 @@ std::vector<std::string> split_string(const std::string& str,
 	const std::string& delimiter) {
 	std::vector<std::string> strings;
 
-	std::string::size_type pos = 0;
+	std::string::size_type pos = 0; 	
 	std::string::size_type prev = 0;
 	while ((pos = str.find(delimiter, prev)) != std::string::npos) {
 		strings.push_back(str.substr(prev, pos - prev));
